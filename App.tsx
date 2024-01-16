eeca624e9d02197daccb162897a9233a748ca840
lordeadroid <rishabhrawat34@gmail.com>
2024-01-16 11:17:56 +0530

    Feature: Added start/pause button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [clicked, setClicked] = useState(false);
+  const handlePress = () => {
+    setClicked(true);
+  };
+  const text = clicked ? 'Pause' : 'START';
+
   return (
     <View style={styles.container}>
-      <Text>Pomodoro</Text>
+      <Text style={styles.logo}>Pomodoro</Text>
+      <View style={styles.button}>
+        <Pressable onPress={handlePress}>
+          <Text style={styles.text}>{text}</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +27,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'flex-start',
     marginTop: 100,
+  },
+  logo: {
+    fontSize: 25,
+    position: 'absolute',
+    top: 10,
+    left: 135,
+  },
+  button: {
+    width: 100,
+    height: 40,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'black',
+    position: 'absolute',
+    top: 300,
+    left: 145,
+  },
+  text: {
+    fontSize: 20,
   },
 });
